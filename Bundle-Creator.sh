80ee8a654c6ef46ce639d0af3c9952b16a09e956
mattintosh4 <mattintosh4@gmail.com>
2012-09-05 19:37:31 +0900

    .

--- Bundle-Creator.sh
+++ Bundle-Creator.sh
@@ -3,7 +3,7 @@
 cd /usr/local/src/rawtherapee
 rm -rf ./build ./Release
 mkdir build && cd $_
-[ -x ~/GitHub/RawTherapee/compile_commands.sh ] && $_ && /opt/local/bin/gmake install -j3
+test -x ~/GitHub/RawTherapee/compile_commands.sh && $_ && /opt/local/bin/gmake install -j3
 cd - > /dev/null && mv ./build/Release . && sed -i "" \
 -e "4s/ccache/gcc-mp-4/" \
 -e "9s/$/ (Development)/" ./Release/AboutThisBuild.txt
@@ -16,10 +16,8 @@ https://github.com/mattintosh4/RawTherapee
 
 Thanks to all developers.
 EOF
-curl -O https://raw.github.com/mattintosh4/RawTherapee/master/patch/start.patch
-curl -O https://raw.github.com/mattintosh4/RawTherapee/master/patch/make-app-bundle.patch
-curl -O https://raw.github.com/mattintosh4/RawTherapee/master/patch/info.plist.patch
-patch -ubN -p0 < start.patch
-patch -ubN -p0 < make-app-bundle.patch
-patch -ubN -p0 < info.plist.patch
+patch -ubN -p0 < ~/GitHub/RawTherapee/patch/start.patch
+patch -ubN -p0 < ~/GitHub/RawTherapee/patch/make-app-bundle.patch
+patch -ubN -p0 < ~/GitHub/RawTherapee/patch/info.plist.patch
+patch -ubN -p0 < ~/GitHub/RawTherapee/patch/config.h.in.patch
 ./tools/osx/make-app-bundle
