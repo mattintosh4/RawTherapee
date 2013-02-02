2816c3a723d599e53018d5023962690d97affc9c
mattintosh4 <mattintosh4@gmail.com>
2013-02-02 22:38:08 +0900

    .

--- tools/osx/macosx_bundle.cmake
+++ tools/osx/macosx_bundle.cmake
@@ -1,8 +1,3 @@
-set (MACOSX_BUNDLE_CONTENTS     "${PROJECT_NAME}.app/Contents")
-set (MACOSX_BUNDLE_MACOS        "${MACOSX_BUNDLE_CONTENTS}/MacOS")
-set (MACOSX_BUNDLE_RESOURCES    "${MACOSX_BUNDLE_CONTENTS}/Resources")
-set (LIBDIR "${MACOSX_BUNDLE_MACOS}/lib")
-set (ETCDIR "${MACOSX_BUNDLE_MACOS}/etc")
 set (BUNDLE_CONTENTS_DIR    "${PROJECT_NAME}.app/Contents")
 set (BUNDLE_MACOS_DIR       "${BUNDLE_CONTENTS_DIR}/MacOS")
 set (BUNDLE_RESOURCES_DIR   "${BUNDLE_CONTENTS_DIR}/Resources")
@@ -18,16 +13,16 @@ if (NOT oldfile STREQUAL "")
 endif ()
 
 
-message (STATUS "Creating app bundle container")
-file (MAKE_DIRECTORY ${MACOSX_BUNDLE_RESOURCES})
+message (STATUS "Creating bundle container")
+file (MAKE_DIRECTORY ${BUNDLE_RESOURCES_DIR})
 
 
 message (STATUS "Copying main files")
-file (COPY "${CMAKE_BUILD_TYPE}" DESTINATION "${MACOSX_BUNDLE_CONTENTS}")
-file (RENAME "${MACOSX_BUNDLE_CONTENTS}/${CMAKE_BUILD_TYPE}" "${MACOSX_BUNDLE_MACOS}")
+file (COPY "${CMAKE_BUILD_TYPE}" DESTINATION "${BUNDLE_CONTENTS_DIR}")
+file (RENAME "${BUNDLE_CONTENTS_DIR}/${CMAKE_BUILD_TYPE}" "${BUNDLE_MACOS_DIR}")
 
 
-function (importDeps)
+function (checkLink)
     execute_process(
         COMMAND otool -L "${ARGV0}"
         COMMAND awk "NR >= 2 && $1 !~ /^(\\/usr\\/lib|\\/System|@executable_path)\\// { print $1 }"
@@ -37,11 +32,8 @@ function (importDeps)
     )
     string (REPLACE "\n" ";" dependencies ${dependencies})
     foreach (x IN LISTS dependencies)
-        get_filename_component (x ${x} REALPATH)
         get_filename_component (y ${x} NAME)
         if (NOT EXISTS "${LIBDIR}/${y}")
-            file (INSTALL "${x}" DESTINATION ${LIBDIR} USE_SOURCE_PERMISSIONS)
-            importDeps (${x})
             file (INSTALL "${x}" DESTINATION "${LIBDIR}" USE_SOURCE_PERMISSIONS)
             if (IS_SYMLINK "${x}")
                 get_filename_component (x ${x} REALPATH)
@@ -51,8 +43,8 @@ function (importDeps)
         endif ()
     endforeach ()
 endfunction ()
-message (STATUS "Copying executable dependencies")
-importDeps ("${MACOSX_BUNDLE_MACOS}/rawtherapee")
+message (STATUS "Copying dependency libraries")
+checkLink ("${BUNDLE_MACOS_DIR}/rawtherapee")
 
 
 ############### Library modules ###############
@@ -70,9 +62,9 @@ file (
     DESTINATION "${ETCDIR}"
     USE_SOURCE_PERMISSIONS
 )
-message (STATUS "Replacing modules config files")
+message (STATUS "Replacing path in the config files")
 execute_process (
-    COMMAND sed -i "" -e "s|${GTK_PREFIX}|/tmp/${MACOSX_BUNDLE_MACOS}|"
+    COMMAND sed -i "" -e "s|${GTK_PREFIX}|/tmp/${BUNDLE_MACOS_DIR}|"
             "${ETCDIR}/gtk-2.0/gdk-pixbuf.loaders"
             "${ETCDIR}/gtk-2.0/gtk.immodules"
             "${ETCDIR}/pango/pango.modules"
@@ -80,17 +72,20 @@ execute_process (
 file (WRITE "${ETCDIR}/pango/pangorc" "[Pango]\nModuleFiles = /tmp/${ETCDIR}/pango/pango.modules")
 
 
-############### Mime data ###############
 message (STATUS "Copying mime data files")
-file (INSTALL "${GTK_PREFIX}/share/mime" DESTINATION "${MACOSX_BUNDLE_MACOS}/share" USE_SOURCE_PERMISSIONS)
+file (INSTALL "${GTK_PREFIX}/share/mime" DESTINATION "${BUNDLE_MACOS_DIR}/share" USE_SOURCE_PERMISSIONS)
 
 
-############### Info.plist ###############
+message (STATUS "Copying other resources")
+file (INSTALL "${PROJECT_SOURCE_DIR}/tools/osx/start" DESTINATION "${BUNDLE_MACOS_DIR}" USE_SOURCE_PERMISSIONS)
+file (INSTALL "${PROJECT_SOURCE_DIR}/tools/osx/Icons.icns" DESTINATION "${BUNDLE_RESOURCES_DIR}" USE_SOURCE_PERMISSIONS)
+
+
 message (STATUS "Creating Info.plist")
 find_program (MERCURIAL hg)
-if (NOT MERCURIAL STREQUAL MERCURIAL-NOTFOUND AND EXISTS ${PROJECT_SOURCE_DIR}/.hg)
+if (NOT MERCURIAL STREQUAL MERCURIAL-NOTFOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.hg")
     execute_process (
-        COMMAND ${MERCURIAL} -R ${PROJECT_SOURCE_DIR} parents --template "{latesttag}.{latesttagdistance}"
+        COMMAND ${MERCURIAL} -R "${PROJECT_SOURCE_DIR}" parents --template "{latesttag}.{latesttagdistance}"
         OUTPUT_VARIABLE
             PROJECT_VERSION
         OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -103,16 +98,12 @@ set (MACOSX_BUNDLE_GUI_IDENTIFIER       "com.${PROJECT_NAME_LOWERCASE}.${PROJECT
 set (MACOSX_BUNDLE_EXECUTABLE_NAME      "start")
 set (MACOSX_BUNDLE_ICON_FILE            "Icons.icns")
 set (MACOSX_BUNDLE_BUNDLE_VERSION       "1.0")
-configure_file ("${CMAKE_ROOT}/Modules/MacOSXBundleInfo.plist.in" "${MACOSX_BUNDLE_CONTENTS}/Info.plist")
+set (MACOSX_BUNDLE_LONG_VERSION_STRING  "${PROJECT_VERSION}")
+#set (MACOSX_BUNDLE_SHORT_VERSION_STRING "")
+string (SUBSTRING "${PROJECT_VERSION}" 0 3 MACOSX_BUNDLE_SHORT_VERSION_STRING)
+configure_file ("${CMAKE_ROOT}/Modules/MacOSXBundleInfo.plist.in" "${BUNDLE_CONTENTS_DIR}/Info.plist")
 
 
-############### Other resources ###############
-message (STATUS "Copying other resources")
-file (INSTALL "${PROJECT_SOURCE_DIR}/tools/osx/start" DESTINATION "${MACOSX_BUNDLE_MACOS}" USE_SOURCE_PERMISSIONS)
-file (INSTALL "${PROJECT_SOURCE_DIR}/tools/osx/Icons.icns" DESTINATION "${MACOSX_BUNDLE_RESOURCES}" USE_SOURCE_PERMISSIONS)
-
-
-############### Distribution disk image ###############
 message (STATUS "Creating distribution disk image")
 execute_process (
     COMMAND hdiutil create -srcdir "${PROJECT_NAME}.app" "${PROJECT_NAME_LOWERCASE}_mac${PROC_BIT_DEPTH}_${PROJECT_VERSION}.dmg"
